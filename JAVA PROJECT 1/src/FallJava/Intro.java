98209c7927fe9783e11685747286f072aa9ac919
judyrodolakis <judyrodolakis@localhost>
2021-09-21 09:31:35 -0400

    Push to git hub

--- JAVA PROJECT 1/src/FallJava/Intro.java
+++ JAVA PROJECT 1/src/FallJava/Intro.java
@@ -2,32 +2,16 @@ package FallJava;
 
 public class Intro {
 	
+	private String myWord;
+	
+	public void setWord(String myWord) {
+}
+	
+	public void sayWord() {
+		String myWord = "hi";
+		System.out.println(myWord);
+}
+
 	public static void main(String[] args) {
-		
-		System.out.println("Hello Judy! How was your summer?");
-		
-		System.out.println("Hi! My summer was awesome! I hung out with friends and played a lot of field hockey.");
-		
-		System.out.println("Cool! Did you go to the beach?");
-		
-		System.out.println("Yes, on Cape Cod.");
-		
-		System.out.println("Did you travel anywhere else?");
-		
-		System.out.println("Not very far but my family and I went on college road trips for my brother. We stayed in New England though.");
-		
-		System.out.println("What is your brothers name? How old is he?");
-		
-		System.out.println("My brothers name is Bobby and he is 18 years old and a senior in High School.");
-		
-		System.out.println("Do you and Bobby tend to get along?");
-		
-		System.out.println("Most of the time... we are extremely competitive.");
-		
-		System.out.println("Hahaha. Do you have anu pets?");
-		
-		System.out.println("Yes. I have a dog name Quinn who I miss very much!");
-		
-		System.out.println("See you later!");
-	}
+	}	
 }
---
+++ JAVA PROJECT 1/src/FallJava/javaBasics.java
@@ -0,0 +1,33 @@
+package FallJava;
+
+public class javaBasics {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Judy! How was your summer?");
+		
+		System.out.println("Hi! My summer was awesome! I hung out with friends and played a lot of field hockey.");
+		
+		System.out.println("Cool! Did you go to the beach?");
+		
+		System.out.println("Yes, on Cape Cod.");
+		
+		System.out.println("Did you travel anywhere else?");
+		
+		System.out.println("Not very far but my family and I went on college road trips for my brother. We stayed in New England though.");
+		
+		System.out.println("What is your brothers name? How old is he?");
+		
+		System.out.println("My brothers name is Bobby and he is 18 years old and a senior in High School.");
+		
+		System.out.println("Do you and Bobby tend to get along?");
+		
+		System.out.println("Most of the time... we are extremely competitive.");
+		
+		System.out.println("Hahaha. Do you have anu pets?");
+		
+		System.out.println("Yes. I have a dog name Quinn who I miss very much!");
+		
+		System.out.println("See you later!");
+	}
+}
