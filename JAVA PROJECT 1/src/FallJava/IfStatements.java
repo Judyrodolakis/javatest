e6585c9f3c2c5c8aa1336d08a800abf3311fd39d
judyrodolakis <judyrodolakis@localhost>
2021-09-28 09:44:01 -0400

    Push to git hub

---
+++ JAVA PROJECT 1/src/FallJava/IfStatements.java
@@ -0,0 +1,34 @@
+package FallJava;
+
+public class IfStatements {
+	
+	public void positive(int x) {
+			if (x > 0) {
+				System.out.println("positive");
+			}
+			else if (x < 0) {
+				System.out.println("negative");
+				}
+			else {
+				System.out.println("zero");
+			}
+				
+	public void evenOdd(int x) {
+			if (x % 2 == 0) {
+				System.out.println("even");
+			}
+			else if (x % 2 == 1) {
+				System.out.println("odd");
+				}
+			else {
+				System.out.println("zero");
+			}
+			
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+	}
+}
