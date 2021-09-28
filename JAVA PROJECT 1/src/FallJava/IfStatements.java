4d02c4d231c9335ec2757ae97ea7fffa2382704e
judyrodolakis <judyrodolakis@localhost>
2021-09-28 09:44:17 -0400

    Push

--- JAVA PROJECT 1/src/FallJava/IfStatements.java
+++ JAVA PROJECT 1/src/FallJava/IfStatements.java
@@ -12,7 +12,7 @@ public class IfStatements {
 			else {
 				System.out.println("zero");
 			}
-				
+	}		
 	public void evenOdd(int x) {
 			if (x % 2 == 0) {
 				System.out.println("even");
@@ -23,9 +23,55 @@ public class IfStatements {
 			else {
 				System.out.println("zero");
 			}
-			
-	public static void main(String[] args) {
+	}
+	
+	public void upperlower(char x) {
 		
+		if (x > 64 && x < 91) {
+			System.out.println("Uppercase");
+		}
+		else if (x > 96 && x < 121) {
+			System.out.println("Lowercase");
+		}	
+		else
+			System.out.println("Neither");
+	}
+	
+	public void ten(int x) {
+		
+		if (x % 10 == 0) {
+			System.out.println("Multiple of ten");
+		}	
+		else
+			System.out.println("Not a multiple");
+	}
+	
+	public void maxFunction(double x, double y, double z) {
+		
+		if (x > y && y > z) {
+			System.out.println("X is the biggest");
+		}
+		else if (y > z && z > x) {
+			System.out.println("Y is the biggest");
+		}	
+		else
+			System.out.println("Z is the biggest");
+	}
+	
+	public static void main(String[] args) {
+	IfStatements runner = new IfStatements();
+		
+		runner.positive(5);
+		
+		runner.evenOdd(9);
+		
+		runner.upperlower('g');
+		
+		runner.ten(7);
+		
+		runner.maxFunction(5,6,8);
+		
+	
 		
 		
 		
